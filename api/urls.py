638f3d5fe99251788168501e8931d4b46ddda63a
paovang <vangpaovang@gmail.com>
2023-12-29 01:15:04 +0700

    update

--- api/urls.py
+++ api/urls.py
@@ -1,23 +1,3 @@
-# from django.urls import path
-# from .views import getAll, getOne, create, update
-
-# urlpatterns = [
-#     path('users', getAll),
-#     path('users/<int:id>', getOne),
-#     path('user/create', create),
-#     path('user/update/<int:id>', update),
-# ]
-
-# from django.urls import path
-# from .views import UserView
-
-# urlpatterns = [
-#     path('create/', UserView.as_view({'post': 'create'}), name='user-create'),
-#     path('update/<int:id>/', UserView.as_view({'put': 'update'}), name='user-update'),
-#     path('getAll/', UserView.as_view({'get': 'get_all'}), name='user-get-all'),
-#     path('getOne/<int:id>/', UserView.as_view({'get': 'get_one'}), name='user-get-one'),
-# ]
-
 from django.urls import path
 from .views import UserView
 
